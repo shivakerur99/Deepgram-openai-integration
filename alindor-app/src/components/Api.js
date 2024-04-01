a6834a35f3f3c550d42be50776bb8f3c16ee5fdc
cruisershiva <shivakerur99@gmail.com>
2024-04-01 19:22:48 +0530

    latest

--- alindor-app/src/components/Api.js
+++ alindor-app/src/components/Api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const Api=axios.create({
-    // baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend
+    baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend
     // baseURL: "https://alindor-ev3t.onrender.com"
 
-    baseURL: "https://alindor-hm.onrender.com",
+    // baseURL: "https://alindor-hm.onrender.com",
 })
 
 export default Api

a91413f96b935050ee609a7179b22867646ebff3
cruisershiva <shivakerur99@gmail.com>
2024-04-02 01:35:46 +0530

    first commit

--- alindor-app/src/components/Api.js
+++ alindor-app/src/components/Api.js
@@ -4,7 +4,7 @@ const Api=axios.create({
     // baseURL: "http://localhost:8000", //for localhost connection with FastAPI backend
     // baseURL: "https://alindor-ev3t.onrender.com"
 
-    baseURL: "https://alindor-hm.onrender.com",
+    baseURL: "https://shivakerur99-alindor-hm.hf.space",
 })
 
 export default Api
